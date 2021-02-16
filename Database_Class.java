234acd94e15ecf82625a331769a5f3c85447ddb9
DSmith1122 <78653846+DSmith1122@users.noreply.github.com>
2021-02-15 19:05:20 -0500

    Update Database_Class.java

--- Database_Class.java
+++ Database_Class.java
@@ -5,6 +5,10 @@
  *  Date: February 12, 2021
  */
 // import statements
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database_Class {
 
     /**
@@ -14,12 +18,36 @@ public class Database_Class {
     private Object maintainAuditLog;
     private Object exportWholeDatabaseAsCSV;
     private Object exportSingleUserAsCSV;
+    
+	private String url = "jdbc:mysql://localhost:3306/MySql";
+	private String username = "root";
+	private String password = "@@t45zr)11";    
 
     /**
      * Constructors
      */
     public Database_Class() {
     }
+    
+	public Connection getConnection() throws SQLException {
+
+		Connection conn = null;
+
+		try {
+
+			conn = DriverManager.getConnection(url, username, password);
+
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+
+			// close();
+
+		}
+
+		return conn;
+
+	}    
 
     /**
      * Getters
