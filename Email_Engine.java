5065dc6f44754b8a8ab9667ed50b4ecb4f90a45b
inyarko <78630206+inyarko@users.noreply.github.com>
2021-02-20 18:53:44 -0800

    Update Email_Engine.java

--- Email_Engine.java
+++ Email_Engine.java
@@ -5,7 +5,55 @@
  *  Date: February 12, 2021
  */
 // import statements
+import java.util.*; 
+import javax.mail.*; 
+import javax.mail.internet.*; 
+import javax.activation.*; 
+import javax.mail.Session; 
+import javax.mail.Transport;
+
+
 public class Email_Engine {
+    
+     public static void main(String [] args)
+    {
+        String recipent = "inyarko1@student.umuc.edu";
+        
+        String sender = "accountcreation495@yahoo.com";
+        
+        String host = "127.0.0.1";
+        
+        Properties properties = System.getProperties();
+        
+        properties.setProperty("smtp.mail.yahoo.com", host);
+        
+    }Session session = Session.getDefaultInstance(properties);
+    
+    
+    try
+    {
+        MimeMessage message = new MimeMessage(session);
+        
+        message.setFrom(new InternetAddress(sender));
+        
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+        
+        message.setSubject("This is Subject");
+        
+        message.setText("This is a test mail");
+        
+        Transport.send(message);
+        System.out.println("Mail Successfully Sent!");
+        
+    }
+    catch (MessagingException mex)
+    }
+
+    mex.printStackTrace();
+
+  }
+}
+}
 
     /**
      * Instance and Field variables
