e67b88ee249b8727bea64b2ff1fd8e0a2d3e4b4d
inyarko <78630206+inyarko@users.noreply.github.com>
2021-03-01 21:00:57 -0800

    Add files via upload

--- main/java/com/acp/Email_Engine.java
+++ main/java/com/acp/Email_Engine.java
@@ -1,3 +1,4 @@
+package com.acp;
 
 /** Purpose: Hold the class controlling the email service
  *  UMGC CMSC 495 Special Topics
@@ -5,62 +6,98 @@
  *  Date: February 12, 2021
  */
 // import statements
-import java.util.*; 
-import javax.mail.*; 
-import javax.mail.internet.*; 
-import javax.activation.*; 
-import javax.mail.Session; 
+import java.util.HashSet;
+import javax.mail.Session;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import java.util.Properties;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 
 
 public class Email_Engine {
     
-     public static void main(String [] args)
-    {
-        String recipent = "inyarko1@student.umuc.edu";
-        
-        String sender = "accountcreation495@yahoo.com";
-        
-        String host = "127.0.0.1";
-        
-        Properties properties = System.getProperties();
-        
-        properties.setProperty("smtp.mail.yahoo.com", host);
-        
-    }Session session = Session.getDefaultInstance(properties);
     
     
-    try
-    {
-        MimeMessage message = new MimeMessage(session);
+    
+     public static void sendMail(String recpient, String confirmationCode) throws Exception {
         
-        message.setFrom(new InternetAddress(sender));
         
-        message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+        //String recipient = "accountcreation495@yahoo.com";
         
-        message.setSubject("This is Subject");
         
-        message.setText("This is a test mail");
         
-        Transport.send(message);
-        System.out.println("Mail Successfully Sent!");
+        System.out.println("Preparing to send email");
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host","smtp.gmail.com");
+        properties.put("mail.smtp.socketFactory.port", "465");
+        properties.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+        properties.put("mail.smtp.port", "465");
         
-    }
-    catch (MessagingException mex)
-    }
+        String sender = "creationprotal495@gmail.com";
+        String password = "Accountcreation123*";
 
-    mex.printStackTrace();
+    Session session = Session.getInstance(properties, new Authenticator(){
+        @Override
+        protected PasswordAuthentication getPasswordAuthentication(){
+            return new PasswordAuthentication(sender, password);
+        }
+    }
+    );
+    
+    
+    Message message = prepareMessage(session, sender, recpient,confirmationCode);
+    
+    Transport.send(message);
+    System.out.println("Message sent successfully!");
+    
+    
+    }
+    
+    private static Message prepareMessage(Session session, String sender, String recpient, String confirmationCode) throws MessagingException{
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(sender));
+            message.setRecipient(Message.RecipientType.TO,new InternetAddress(recpient));
+            message.setSubject("Lets Verify Your Account Creation Portal");
+          /*  Random rand = new Random();
+        HashSet<Integer> set = new HashSet<>();
+        while(set.size()< 1){
+            int random=rand.nextInt(999999) + 10;
+            set.add(random);
+            
+        }for (int randomNumber:set){
+            System.out.println("Here is you verification number: " + randomNumber); */
+            message.setText("Thank you for registering. Here is you verification number: " + confirmationCode);
+      //  }
+           // message.setText("This is a Test mail" + randomNumber);
+            
+            return message;
+        } catch (Exception ex) {
+            Logger.getLogger(Email_Engine.class.getName()).log(Level.SEVERE, null, ex);
+        }    
+    return null;
+    }  
+}    
 
-  }
-}
-}
+    
 
     /**
      * Instance and Field variables
      */
     /**
      * Constructors
-     */
+     
     public Email_Engine() {
     }
 
@@ -72,7 +109,8 @@ public class Email_Engine {
      */
     /**
      * Methods
-     */
+     
     public void sendTheEmail() {
     }
 }
+*/
